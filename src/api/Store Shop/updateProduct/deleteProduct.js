2a63a581d717de6f79ee684b522d42bc00bdf8e4
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-11-28 10:28:44 +0600

    delete product and update a store limit

--- src/api/Store Shop/updateProduct/deleteProduct.js
+++ src/api/Store Shop/updateProduct/deleteProduct.js
@@ -1,11 +1,19 @@
-
+const Store = require('../../../modal/Store')
 
 const Product = require("../../../modal/Product");
 
 const deleteItem = async (req, res) => {
     const id = req.params?.id;
-
+    const count = req.query?.count
+    const email = req.query?.email
+    const limit = parseInt(count)
     try {
+        const query = { email: email };
+        const isExist = await Store.findOne(query)
+        const updateDoc = {
+            $set: {limit: isExist?.limit + limit}
+        }
+        let UpdateUserLimit = await Store.updateOne(query, updateDoc);
         const result = await Product.deleteOne({ _id: id });
         res.send(result)
     } catch (error) {
